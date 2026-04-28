37bb7d1f9cf6047844985d4560f4aa7d6da233c6
Mubashir Saeed <mubashirsaeed234@gmail.com>
2026-04-28 17:12:08 +0500

    isAdmin middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
+import isAdmin from "../middleware/isAdmin.js";
 import  {register, Users , login, profile, logout} from "../controllers/authController.js";
 const router = express.Router();
 
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/users", authMiddleware, Users);
+router.get("/users", authMiddleware, isAdmin, Users);
 router.get("/profile", authMiddleware, profile);
 router.post("/logout",authMiddleware, logout);
 
